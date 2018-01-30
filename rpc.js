ac09358047db1b635f2f9ac36d8a28ba8c424c52
Daniel Cousens <github@dcousens.com>
2018-01-30 17:14:51 +1100

    default to localhost

--- rpc.js
+++ rpc.js
@@ -1,5 +1,5 @@
 module.exports = require('yajrpc/qup')({
-  url: process.env.RPC,
+  url: process.env.RPC || 'http://localhost:8332',
   auth: require('fs').readFileSync(process.env.RPCCOOKIE),
   batch: process.env.RPCBATCHSIZE || 500,
   concurrent: process.env.RPCCONCURRENT || 16
